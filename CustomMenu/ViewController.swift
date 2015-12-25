57dee978ac486d6040eb8bd46c9a78637a28a810
sashablokhin <sashablokhindev@gmail.com>
2015-12-25 13:09:00 +0300

    Initial project

---
+++ CustomMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomMenu
+//
+//  Created by Alexander Blokhin on 25.12.15.
+//  Copyright © 2015 Alexander Blokhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
