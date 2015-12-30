63e1c88872a83a74880dcbfed8a413aa6c809132
sashablokhin <sashablokhindev@gmail.com>
2015-12-30 17:20:51 +0300

    Animated transitioning complete

--- CustomMenu/MenuAnimatedTransitioning.swift
+++ CustomMenu/MenuAnimatedTransitioning.swift
@@ -31,23 +31,8 @@ class MenuAnimatedTransitioning: NSObject, UIViewControllerAnimatedTransitioning
         let menuView = menuViewController.view
         let bottomView = bottomViewController.view
         
-        // setup 2D transitions for animations
-        let offstageLeft = CGAffineTransformMakeTranslation(-150, 0)
-        let offstageRight = CGAffineTransformMakeTranslation(150, 0)
-        
-        // prepare the menu
-        if (self.presenting){
-            
-            // prepare menu to fade in
-            menuView.alpha = 0
-            
-            // prepare menu items to slide in
-            menuViewController.cameraImageView.transform = offstageLeft
-            menuViewController.cameraLabel.transform = offstageLeft
-            
-            menuViewController.compassImageView.transform = offstageRight
-            menuViewController.compassLabel.transform = offstageRight
-            
+        if (self.presenting) {
+            self.offStageMenuController(menuViewController)
         }
         
         // add the both views to our view controller
@@ -59,29 +44,13 @@ class MenuAnimatedTransitioning: NSObject, UIViewControllerAnimatedTransitioning
         // perform the animation!
         UIView.animateWithDuration(duration, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: [], animations: {
             
-                if (self.presenting){
+                if (self.presenting) {
                     // fade in
-                    menuView.alpha = 1
-                
-                    // onstage items: slide in
-                    menuViewController.cameraImageView.transform = CGAffineTransformIdentity
-                    menuViewController.cameraLabel.transform = CGAffineTransformIdentity
-                
-                    menuViewController.compassImageView.transform = CGAffineTransformIdentity
-                    menuViewController.compassLabel.transform = CGAffineTransformIdentity
-                
+                    self.onStageMenuController(menuViewController)
                 }
                 else {
                     // fade out
-                    menuView.alpha = 0
-                
-                    // offstage items: slide out
-                    menuViewController.cameraImageView.transform = offstageLeft
-                    menuViewController.cameraLabel.transform = offstageLeft
-                
-                    menuViewController.compassImageView.transform = offstageRight
-                    menuViewController.compassLabel.transform = offstageRight
-                
+                    self.offStageMenuController(menuViewController)
                 }
             
             }, completion: { finished in
@@ -91,15 +60,71 @@ class MenuAnimatedTransitioning: NSObject, UIViewControllerAnimatedTransitioning
                 
                 // bug: we have to manually add our 'to view' back 
                 UIApplication.sharedApplication().keyWindow?.addSubview(screens.to.view)
-                
         })
-        
     }
     
     // return how many seconds the transiton animation will take
     func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
         return 0.5
     }
+    
+    // MARK: - Supporting functions
+    
+    func offStageMenuController(menuViewController: MenuViewController) {
+        menuViewController.view.alpha = 0
+        
+        // setup parameters for 2D transitions for animations
+        let topRowOffset  :CGFloat = 300
+        let middleRowOffset :CGFloat = 150
+        let bottomRowOffset  :CGFloat = 50
+        
+        menuViewController.cameraImageView.transform = self.offStage(-topRowOffset)
+        menuViewController.cameraLabel.transform = self.offStage(-topRowOffset)
+        
+        menuViewController.compassImageView.transform = self.offStage(topRowOffset)
+        menuViewController.compassLabel.transform = self.offStage(topRowOffset)
+        
+        menuViewController.markerImageView.transform = self.offStage(-middleRowOffset)
+        menuViewController.markerLabel.transform = self.offStage(-middleRowOffset)
+        
+        menuViewController.plannerImageView.transform = self.offStage(middleRowOffset)
+        menuViewController.plannerLabel.transform = self.offStage(middleRowOffset)
+        
+        menuViewController.routeImageView.transform = self.offStage(-bottomRowOffset)
+        menuViewController.routeLabel.transform = self.offStage(-bottomRowOffset)
+        
+        menuViewController.suitcaseImageView.transform = self.offStage(bottomRowOffset)
+        menuViewController.suitcaseLabel.transform = self.offStage(bottomRowOffset)
+    }
+    
+    func onStageMenuController(menuViewController: MenuViewController){
+        // prepare menu to fade in
+        menuViewController.view.alpha = 1
+        
+        menuViewController.cameraImageView.transform = CGAffineTransformIdentity
+        menuViewController.cameraLabel.transform = CGAffineTransformIdentity
+        
+        menuViewController.compassImageView.transform = CGAffineTransformIdentity
+        menuViewController.compassLabel.transform = CGAffineTransformIdentity
+        
+        menuViewController.markerImageView.transform = CGAffineTransformIdentity
+        menuViewController.markerLabel.transform = CGAffineTransformIdentity
+        
+        menuViewController.plannerImageView.transform = CGAffineTransformIdentity
+        menuViewController.plannerLabel.transform = CGAffineTransformIdentity
+        
+        menuViewController.routeImageView.transform = CGAffineTransformIdentity
+        menuViewController.routeLabel.transform = CGAffineTransformIdentity
+        
+        menuViewController.suitcaseImageView.transform = CGAffineTransformIdentity
+        menuViewController.suitcaseLabel.transform = CGAffineTransformIdentity
+    }
+    
+    
+    func offStage(amount: CGFloat) -> CGAffineTransform {
+        return CGAffineTransformMakeTranslation(amount, 0)
+    }
+    
     
     // MARK: - UIViewControllerTransitioningDelegate
     
